7f9f59ec8892d630887fb23be42799abeb836f8d
SiteRelEnby <SiteRelEnby@localhost>
2023-02-28 21:27:46 -0600

    use an entrypoint script to try to locate build-all.sh instead of hardcoding it

--- Dockerfile
+++ Dockerfile
@@ -14,10 +14,15 @@ RUN abuild -F checksum \
 
 FROM alpine:3.15
 
+COPY entrypoint.sh /entrypoint.sh
+
 RUN --mount=from=avr-libc-builder,source=/root/packages,target=/packages \
   apk --no-cache --allow-untrusted add bash perl make /packages/*/avr-libc*.apk
 
 ENV ATTINY_DFP=/not/existing
 #RUN mkdir -p /src/ToyKeeper/spaghetti-monster/anduril
-WORKDIR /src/ToyKeeper/spaghetti-monster/anduril
-ENTRYPOINT ["./build-all.sh"]
+#WORKDIR /src/ToyKeeper/spaghetti-monster/anduril
+RUN mkdir /src
+WORKDIR /src
+#ENTRYPOINT ["./build-all.sh"]
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+[[ "${DEBUG}" == "1" ]] && set -x
+set -e
+set -o pipefail
+[[ ! -d /src ]] && exit 1
+
+if [[ -f /src/spaghetti-monster/anduril/build-all.sh ]]
+then
+	cd /src/spaghetti-monster/anduril
+	exec ./build-all.sh ${*}
+else
+	for dirpath in anduril anduril2 ToyKeeper/anduril
+	do
+		if [[ -d "/src/${dirpath}" ]]
+		then
+		cd ${dirpath}
+			if [[ -f "spaghetti-monster/anduril/build-all.sh" ]]
+			then
+				cd spaghetti-monster/anduril
+				exec ./build-all.sh ${*}
+			elif [[ -f "anduril/build-all.sh" ]] 	#put this case here to catch any restructured source. May be unnecessary but catches an edge case.
+								#if the user mounts this dir into /src from the default layout it will fail though as fsm in ../ won't get pulled in.
+			then
+				cd anduril
+				exec ./build-all.sh ${*}
+			fi
+		else
+			continue
+		fi
+	done
+fi
+echo "nothing in /src looks like an anduril source directory." >&2
+[[ "${DEBUG}" == "1" ]] && find /src >&2
+exit 1
